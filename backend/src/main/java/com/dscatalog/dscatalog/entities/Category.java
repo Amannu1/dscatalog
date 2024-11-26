5d1ffee97c434f78323be70cf1b3102f2d82b6cd
mannuuella <manuercolle@gmail.com>
2024-11-26 20:29:02 -0300

    CategoryResource

---
+++ backend/src/main/java/com/dscatalog/dscatalog/entities/Category.java
@@ -0,0 +1,46 @@
+package com.dscatalog.dscatalog.entities;
+
+import jakarta.persistence.Id;
+import java.util.Objects;
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
