ec4d6406b3967f4da0e95961ae03ae877b9d5bbe
Luiz <manuercolle@gmail.com>
2025-03-06 19:35:47 -0300

    Fixture repository test implement

--- backend/src/test/java/com/dscatalog/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/dscatalog/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.dscatalog.dscatalog.repositories;
 
 import com.dscatalog.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,10 +15,15 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+    private long existingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-
-        long existingId = 1L;
 
         repository.deleteById(existingId);
 
