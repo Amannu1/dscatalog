1713102f771a02b41d1df08b23f56f5daf92032c
Luiz <manuercolle@gmail.com>
2025-04-10 15:21:57 -0300

    EmailService and exception

---
+++ backend/src/main/java/com/dscatalog/dscatalog/services/exceptions/EmailException.java
@@ -0,0 +1,9 @@
+package com.dscatalog.dscatalog.services.exceptions;
+
+@SuppressWarnings("serial")
+public class EmailException extends RuntimeException {
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
