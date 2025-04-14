53f2df3d010435eefe7744a47c2f22ed35a03d4e
Luiz <manuercolle@gmail.com>
2025-04-14 15:09:10 -0300

    Email not found validation

--- backend/src/main/java/com/dscatalog/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/dscatalog/dscatalog/services/AuthService.java
@@ -1,12 +1,25 @@
 package com.dscatalog.dscatalog.services;
 
 import com.dscatalog.dscatalog.dto.EmailDTO;
-import jakarta.validation.Valid;
+import com.dscatalog.dscatalog.entities.User;
+import com.dscatalog.dscatalog.repositories.UserRepository;
+import com.dscatalog.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Transactional
     public void createRecoverToken(EmailDTO body) {
 
+        User user = userRepository.findByEmail(body.getEmail());
+        if(user == null) {
+            throw new ResourceNotFoundException("Email not found");
+        }
     }
 }
