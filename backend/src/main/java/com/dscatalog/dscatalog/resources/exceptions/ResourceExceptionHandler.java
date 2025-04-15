d6b5fbbbed42cdae1d78909ad2c9cff639ac63a1
Luiz <manuercolle@gmail.com>
2025-04-15 15:39:38 -0300

    Email exception

--- backend/src/main/java/com/dscatalog/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/dscatalog/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.dscatalog.dscatalog.resources.exceptions;
 
 import com.dscatalog.dscatalog.services.exceptions.DatabaseException;
+import com.dscatalog.dscatalog.services.exceptions.EmailException;
 import com.dscatalog.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -54,6 +55,18 @@ public class ResourceExceptionHandler {
             err.addError(f.getField(), f.getDefaultMessage());
         }
 
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(EmailException.class)
+    public ResponseEntity<StandardError> email(EmailException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Email exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
